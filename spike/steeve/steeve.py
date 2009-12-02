e82ab4058e4c347f47e7c8462b64f28ef84b73a4
Steeve BARBEAU <steeve@laptop.local>
2009-12-02 09:20:08 +0100

    Test Entangled DHT

--- spike/steeve/steeve.py
+++ spike/steeve/steeve.py
@@ -24,31 +24,101 @@ def main():
     myId = 'steeve1234'
     #création du datastore pour stocker les infos à partager
     myDataStore = DictDataStore()
+    # on lui attribue des éléments
     myDataStore.setItem('key1', 'value1', time.time(), None, myId)
     myDataStore.setItem('key2', 'value2', time.time(), None, myId)
-    print str(myDataStore.keys())
-    print myDataStore.__getitem__('key1')
-    myDataStore.__delitem__('key2')
-    print str(myDataStore.keys())
+    myDataStore.setItem('key3', 'value3', time.time(), None, myId)
+    myDataStore.setItem('key4', 'value4', time.time(), None, myId)
+
+    print str(myDataStore.keys())			# affichage des clés du datastore
+    print myDataStore.__getitem__('key1')	# affichage de la valeur de la clé demandée
+
+    myDataStore.__delitem__('key2')			# suppression de l'élément
+    myDataStore.setItem('key5', 'value5', time.time(), None, myId)	# ajout d'une élément
+    print str(myDataStore.keys())			# affichage des clés du datastore
+
 
     #création de ma table de routage
     myTreeRoutingTable = TreeRoutingTable(myId)
 
     # mon noeud
-    myNode = Node(8600, myDataStore, myTreeRoutingTable, None)
-    myNode.printContacts()
+    myNode = Node(8600, myDataStore, None, None)
 
     # protocole
     myProto = KademliaProtocol(myNode)
 
     # mon premier contact
-    myFirstContact = Contact('contact111', '192.168.1.11', 1111, myProto)
+    myFirstContact = Contact('contact111', '192.168.1.1', 1111, myProto)    
+    # mon second contact
+    mySecondContact = Contact('contact222', '192.168.1.12', 1111, myProto)
+    # mon troisieme contact
+    myThirdContact = Contact('contact333', '192.168.1.13', 1111, myProto)    
 
-    # ajout d'un contact
+
+    # manipulation des contacts au niveau du noeud "myNode"
+    print "\n\n\tPremier Noeud \n - ajout de 2 contacts"
+    myNode.addContact(myFirstContact)
+    myNode.addContact(mySecondContact)
+    print " - affichage des contacts du noeud"
+    myNode.printContacts()					# affichage de tous les contacts du noeud
+    print " - suppression du contact : contact111"
+    myNode.removeContact('contact111')                           # remove inefficace 
+    print " - affichage des contacts du noeud"
+    myNode.printContacts()		# on peut voir que la suppression du contact : contact111 n'a pas fonctionné
+
+    print " - recherche du contact : contact111"
+    print myNode.findContact('contact111')					# retourne un Deferred cf Twisted, pk ???
+    print " - recherche du noeud : contact222"			### ????? QU'EST CE QU'IL RETOURNE ??????????????????????  
+    print myNode.findNode('contact222')
+    print " - stockage de datas"
+    myNode.store('datakey1', 'datavalue1', myId)
+    myNode.store('datakey2', 'datavalue2', myId)
+    myNode.store('datakey3', 'datavalue3', myId)
+    print "recherche de datas : datakey1"
+    print myNode.findValue('datakey1')				#recherche de datas
+
+
+
+    print "\n\n\n\n Manipulation de la table de routage créée manuellement"
+    # ajout de 2 contacts dans notre table de routage créée manuellement
     myTreeRoutingTable.addContact(myFirstContact)
-    print str(myTreeRoutingTable.getContact('contact111'))
-    myNode.printContacts()
+    myTreeRoutingTable.addContact(mySecondContact)
+    myTreeRoutingTable.addContact(myThirdContact)
+    print "contact111 :"
+    print str(myTreeRoutingTable.getContact('contact111'))	# affichage des informations concernant le contact : contact111
+    myTreeRoutingTable.removeContact('contact111')
+    print "contact222 :"
+    print str(myTreeRoutingTable.getContact('contact222'))
+    print "les 3 noeuds les plus proches de contact111 :"
+    print str(myTreeRoutingTable.findCloseNodes('contact111', 3)) # retourne les noeuds les plus proches sous forme d'une liste
+
+
+    """print "\n\n\nSecond node\n##################"
+    # un second noeud avec lequel on va essayer de communiquer
+    yourNode = Node(7885, myDataStore, None, None)
+	# son premier contact
+    yourFirstContact = Contact('contact888', '192.168.1.85', 1234, myProto)
+    yourNode.addContact(yourFirstContact)
+    # son second contact
+    yourSecondContact = Contact('contact555', '192.168.1.78', 4321, myProto)
+    yourNode.addContact(yourSecondContact)
+    yourNode.printContacts()"""
+
+    a = [('127.0.0.1', 6666)]
+    myNode.joinNetwork(a)			# pour se connecter au noeud 127.0.0.1 port UDP 6666
+    #twisted.internet.reactor.run()
     
+    # exemple quand on utilise un table de routage existante (erreur car dans __init__ il n'initialise pas cette table, il le fait seulement lorsque qu'on laisse None comme RootingTable)
+    #myNode2 = Node(8602, myDataStore, myTreeRoutingTable, None)
+    #myNode2.printContacts()
+
+
+
+
+# probleme avec khashmir : pleins de modifs a faire, car ça a été codé avec une vieille version de python, et pb avec twisted : il faut importer "app" de "twisted.internet" mais il ne doit plus exister
+
+
+
 
 main()
 
